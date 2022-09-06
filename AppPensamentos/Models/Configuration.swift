114361246bfea287d6b80774964dc428a93c150f
Renato Dias <renatocardosodias@gmail.com>
2022-09-06 17:53:34 -0300

    let defaults = armazendo na variavel defaults as preferencias do usuario static var shared = criando uma variável da instancia Configuration private init = construtor privado da classe Configuration

--- AppPensamentos/Models/Configuration.swift
+++ AppPensamentos/Models/Configuration.swift
@@ -9,7 +9,15 @@ import Foundation
 
 class Configuration {
     //Persistindo dados com UserDefaults
-    //armazendo na variavel defaults
+    //armazendo na variavel defaults as preferencias do usuario
     let defaults = UserDefaults.standard
+    //configurando uma variavel global
+    static var shared: Configuration = Configuration()
+    
+    //construindo um inicializador privado
+    private init() {
+        
+    }
+    
     
 } //end class Configuration
