8e40930c65b36daf0c415fb90d331d2b833efe40
Renato Dias <renatocardosodias@gmail.com>
2022-09-06 18:04:16 -0300

    criando um enumerador com os dados a ser recuperados recuperando o valor real usando a função UserDefaultsKeys.[enum].rawValue

--- AppPensamentos/Models/Configuration.swift
+++ AppPensamentos/Models/Configuration.swift
@@ -7,12 +7,25 @@
 
 import Foundation
 
+enum UserDefaultsKeys: String{
+    case timeInterval = "timeInterval"
+    case colorScheme = "colorScheme"
+    case autorefresh = "autorefresh"
+}
+
 class Configuration {
     //Persistindo dados com UserDefaults
     //armazendo na variavel defaults as preferencias do usuario
     let defaults = UserDefaults.standard
     //configurando uma variavel global
     static var shared: Configuration = Configuration()
+    
+    var timeInterval: Double {
+        get {
+            //recuprando uma informação
+            return defaults.double(forKey: UserDefaultsKeys.timeInterval.rawValue)
+        }
+    }
     
     //construindo um inicializador privado
     private init() {
