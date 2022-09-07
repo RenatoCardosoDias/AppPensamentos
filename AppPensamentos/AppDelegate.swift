42001074d2b08955e0df4cdcdd91c423c4fcbbf0
Renato Dias <renatocardosodias@gmail.com>
2022-09-07 06:38:22 -0300

    informar ao aplicativo que houve uma alteração

--- AppPensamentos/AppDelegate.swift
+++ AppPensamentos/AppDelegate.swift
@@ -30,7 +30,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
+    //informar ao aplicativo que houve uma alteração
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "Refresh"), object: nil)
+    }
 
 }
 
